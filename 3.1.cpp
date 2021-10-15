fa3302cc1c9ec3e6945a19520816f48aaede36fd
ZizTo <92594525+ZizTo@users.noreply.github.com>
2021-10-15 19:12:31 +0300

    This is first version of 3 lesson
    Boolean1-2

---
+++ 3.1.cpp
@@ -0,0 +1,27 @@
+﻿// Матиевский Павел
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	//		 Дано целое число A. 
+	//Проверить истинность высказывания: 
+	//«Число A является положительным».
+	/*int a;
+	bool res;
+	cin >> a;
+	res = a > 0;
+	cout << res << endl;*/
+
+	//		Дано целое число A. 
+	//Проверить истинность высказывания: 
+	//«Число A является нечетным».
+	int a; bool res;
+	cin >> a;
+	res = (a % 2) != 0;
+	cout << res << endl;
+
+	return 0;
+}
